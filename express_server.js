50823dc9410007d6322ecc642e6af37f01295819
Matthew Cook <matthewcook13987@gmail.com>
2021-02-15 23:22:48 +0000

    Began implementing code to redirect the user from the database to the actual links provided.

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 const { request } = require("express");
+const bodyParser = require('body-parser');
 
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -12,12 +12,12 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
-const generateRandomString = function () {
+const generateRandomString = function() {
   //returns 6 random chars
-  return Math.random().toString(16).substr(2, 6)
-}
+  return Math.random().toString(16).substr(2, 6);
+};
 
 
 app.get('/', (req, res) => {
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars)
-})
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
@@ -40,7 +40,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -48,16 +48,17 @@ app.get("/hello", (req, res) => {
 
 
 app.post('/urls', (req, res) => {
-  //users["5315"] = {first_name: "John", last_name: "Smith"}
-  
   const shortURL = generateRandomString();
 
   urlDatabase[shortURL] = req.body.longURL;
 
-  //console.log(req.body);
-  //console.log(req.params)
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase.longURL;
+  res.redirect(longURL);
+});
 
 
 app.listen(PORT, () => {
