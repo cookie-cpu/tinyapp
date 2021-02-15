139fbd51c6d618d11a26036915966db891eff6dd
Matthew Cook <matthewcook13987@gmail.com>
2021-02-15 22:27:40 +0000

    Added body parser and began implementation of post method to retrieve data from the submitted form.

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine","ejs");
 
 const urlDatabase = {
@@ -22,6 +25,10 @@ app.get('/urls', (req,res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
@@ -34,6 +41,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+app.post('/urls', (req,res) => {
+  console.log(req.body);
+  res.send("OK!")
+})
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
