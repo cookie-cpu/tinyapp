3ce3ffc7fd3295e2f956b1d964ba5862a4524a9e
Matthew Cook <matthewcook13987@gmail.com>
2021-02-18 02:05:22 +0000

    changed urlDatabase data structure to include the user id with every link. Made adjustments throughout code to correctly display links given new data format.

--- express_server.js
+++ express_server.js
@@ -13,9 +13,13 @@ app.set("view engine", "ejs");
 
 
 //Global Objects
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 const users = {
   "userRandomID": {
@@ -77,7 +81,7 @@ app.get("/users.json", (req, res) => {
 
 //Redirects the user to the actual longurl webpage based on the shorturl
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
@@ -122,7 +126,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user: users[req.cookies["user_id"]] };
   res.render("urls_show", templateVars);
 });
